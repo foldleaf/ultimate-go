8e9eb4465ed6d515fe9665bd0754ec6c1ffd3ce2
foldleaf <59046808+foldleaf@users.noreply.github.com>
2023-02-03 16:19:52 +0800

    添加翻译

--- go/language/map.go
+++ go/language/map.go
@@ -14,9 +14,11 @@ func main() {
 	// ----------------------
 
 	// Declare and make a map that stores values of type user with a key of type string.
+	// 声明并用make函数创建一个 map(映射)，存储 user类型的值(value)与string类型的键(key) 
 	users1 := make(map[string]user)
 
 	// Add key/value pairs to the map.
+	// 向 map 中添加键/值对.
 	users1["Roy"] = user{"Rob", "Roy"}
 	users1["Ford"] = user{"Henry", "Ford"}
 	users1["Mouse"] = user{"Mickey", "Mouse"}
@@ -24,6 +26,7 @@ func main() {
 
 	// ----------------
 	// Iterate over map
+	// map的循环
 	// ----------------
 
 	fmt.Printf("\n=> Iterate over map\n")
@@ -33,6 +36,7 @@ func main() {
 
 	// ------------
 	// Map literals
+	// map 的遍历
 	// ------------
 
 	// Declare and initialize the map with values.
@@ -51,17 +55,21 @@ func main() {
 
 	// ----------
 	// Delete key
+	// 删除键
 	// ----------
 
 	delete(users2, "Roy")
 
 	// --------
 	// Find key
+	// 查找键
 	// --------
 
 	// Find the Roy key.
 	// If found is True, we will get a copy value of that type.
 	// if found is False, u is still a value of type user but is set to its zero value.
+	// 如果found为true，则获得一个那个key类型（user类型的值）的拷贝值
+	// 如果found为false，则是获得user类型的零值
 	u1, found1 := users2["Roy"]
 	u2, found2 := users2["Ford"]
 
@@ -72,6 +80,7 @@ func main() {
 
 	// --------------------
 	// Map key restrictions
+	// Map 键约束
 	// --------------------
 
 	// type users []user
@@ -82,4 +91,10 @@ func main() {
 	// the compiler says we cannot use that: "invalid map key type users"
 	// The reason is: whatever we use for the key, the value must be comparable. We have to use it
 	// in some sort of boolean expression in order for the map to create a hash value for it.
+	// 使用这种语法，我们可以定义一组 user
+	// 这是定义users的第二种方式.我们可以使用现有的类型并将其作为另一种类型。
+	// 这是两种不同的类型. 它们之间没有关系
+	// 但是，当我们尝试使用它作为key时,比如：u: = make (map [ users ] int)
+	// 编译器说我们不能使用"invalid map key type users"
+	// 原因是：无论我们使用什么key，它的value都是可比较的，我们必须在某种布尔表达式中使用它，以便 map为其创建哈希值
 }
