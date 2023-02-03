9a58b3f347464dff454b1828a89c63e57eea0eb7
foldleaf <59046808+foldleaf@users.noreply.github.com>
2023-02-03 16:23:25 +0800

    Update function.go

--- go/language/function.go
+++ go/language/function.go
@@ -35,6 +35,9 @@ func main() {
 	// Update user name. Don't care about the update stats.
 	// This _ is called blank identifier.
 	// Since we don't need anything outside the scope of if, we can use the compact syntax.
+	// 更新 user的 name. 不需要关系 stats的更新.
+	// 这个 _ 是空标识符
+	// 因为我们不需要if作用域之外的东西，所以可以用紧凑的语法.
 	if _, err := updateUser(u); err != nil {
 		fmt.Println(err)
 		return
@@ -46,8 +49,10 @@ func main() {
 
 // retrieveUser retrieves the user document for the specified user.
 // It takes a string type name and returns a pointer to a user type value and bool type error.
+// 它接收一个 string 类型的形参 name，返回一个指向 user 类型的指针以及bool类型的 error.
 func retrieveUser(name string) (*user, error) {
 	// Make a call to get the user in a json response.
+	// 调用 getUser获取 user ，得到 json 响应（string类型）.
 	r, err := getUser(name)
 	if err != nil {
 		return nil, err
@@ -55,15 +60,20 @@ func retrieveUser(name string) (*user, error) {
 
 	// Goal: Unmarshal the json document into a value of the user struct type.
 	// Create a value type user.
+	// 目的: 将 json 文档解析为 user 结构体类型的值.
 	var u user
 
 	// Share the value down the call stack, which is completely safe so the Unmarshal function can
 	// read the document and initialize it.
+	// 在调用栈中共享值，这是完全安全的，因此Unmarshal函数可以读取JSON文档并初始化它。
 	err = json.Unmarshal([]byte(r), &u)
 
 	// Share it back up the call stack.
 	// Because of this line, we know that this create an allocation.
 	// The value is the previous step is not on the stack but on the heap.
+	// 将其共享回调用堆栈。
+	// 这一行，我们知道进行了一次分配。
+	// 该赋值为前一步，不是在栈上而是在堆上
 	return &u, err
 }
 
@@ -75,6 +85,7 @@ func getUser(name string) (string, error) {
 }
 
 // updateUser updates the specified user document.
+// GetUser 模拟 Web 调用返回指定user的 JSON文档
 func updateUser(u *user) (*updateStats, error) {
 	// response simulates a JSON response.
 	response := `{"Modified":1, "Duration":0.005, "Success" : true, "Message": "updated"}`
